0cdd4fa879d4551ae1215d37447bbf5fb8b6454b
Touheed Naveed <braja7059@gmail.com>
2024-09-12 20:42:57 +0500

    bugs fixes

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import {withSentryConfig} from '@sentry/nextjs';
 const nextConfig = {
     output: 'export',
     typescript:{
-        ignoreBuildErrors:true,
+        ignoreBuildErrors: true,
     }
 };
 
